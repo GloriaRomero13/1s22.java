8c663357073bda3a855cb7373e0bde506497aa1a
GloriaRomero13 <144244363+GloriaRomero13@users.noreply.github.com>
2023-09-18 14:24:57 -0600

    Add files via upload

---
+++ p1_1s22.java
@@ -0,0 +1,7 @@
+class p1_1s22{
+	  public static void main(String[] args) {
+	    System.out.println("estamos aprendiendo a programar");
+	    System.out.println("esta es una segunda linea");
+	  }
+
+}
---
+++ p2_1s22.java
@@ -0,0 +1,10 @@
+class p2_1s22{
+	  public static void main(String[] args) {
+	    System.out.println("Gloria Guadalupe Romero Hernandez");
+	    System.out.println("21 años");
+	    System.out.println("gloria.guadalupe.r.h@gmail.com");
+	    System.out.println("leer, ver gameplays y jugar videojuegos");
+	    System.out.println("azul");
+	  }
+
+}
